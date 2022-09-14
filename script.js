b42e4c2d13295446dce86f1416a1004449355bc2
sidratulshatil <105915771+sidratulshatil@users.noreply.github.com>
2022-09-15 00:41:15 +0600

    simple bugs fixed

--- script.js
+++ script.js
@@ -86,12 +86,12 @@ const gameOver = () => {
   // show result
   resultModal.innerHTML += `
     <h1>Finished!</h1>
-    <p>You took: <span class="bold">${timeTaken}</span> seconds</p>
+    <p>You took: <span class="bold">${parseInt(timeTaken)}</span> seconds</p>
     <p>You made <span class="bold red">${errorCount}</span> mistakes</p>
     <button onclick="closeModal()">Close</button>
   `;
 
-  addHistory(questionText, timeTaken, errorCount);
+  addHistory(questionText, parseInt(timeTaken), errorCount);
 
   // restart everything
   startTime = null;
